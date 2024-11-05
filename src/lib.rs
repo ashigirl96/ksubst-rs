1a370863c382cd41c1d2718e4edf9fa373ac374c
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2024-11-05 15:02:09 +0900

    Fix keyword

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //! all matching placeholders.
 //!
 //! Its name and logic is similar to the [`envsubst`] GNU utility, but
-//! this only supports braces-delimited variables (i.e. `${foo}`) and
+//! this only supports braces-delimited variables (i.e. `K{foo}`) and
 //! takes replacement values from an explicit map of variables.
 //!
 //! [`envsubst`]: https://www.gnu.org/software/gettext/manual/html_node/envsubst-Invocation.html
@@ -13,17 +13,17 @@
 //! ## Example
 //!
 //! ```rust
-//! let base_url = "${protocol}://${hostname}/${endpoint}";
-//! assert!(envsubst::is_templated(base_url));
+//! let base_url = "K{protocol}://K{hostname}/K{endpoint}";
+//! assert!(ksubst::is_templated(base_url));
 //!
 //! let mut context = std::collections::HashMap::new();
 //! context.insert("protocol".to_string(), "https".to_string());
 //! context.insert("hostname".to_string(), "example.com".to_string());
 //! context.insert("endpoint".to_string(), "login".to_string());
-//! assert!(envsubst::validate_vars(&context).is_ok());
+//! assert!(ksubst::validate_vars(&context).is_ok());
 //!
-//! let final_url = envsubst::substitute(base_url, &context).unwrap();
-//! assert!(!envsubst::is_templated(&final_url));
+//! let final_url = ksubst::substitute(base_url, &context).unwrap();
+//! assert!(!ksubst::is_templated(&final_url));
 //! assert_eq!(final_url, "https://example.com/login");
 //! ```
 
@@ -39,7 +39,7 @@ pub struct Error(String);
 
 /// Substitute variables in a template string with optional suffix handling.
 ///
-/// This function replaces tokens of the form `${VAR}`, `${VAR.}`, `${VAR-}` in the template string.
+/// This function replaces tokens of the form `K{VAR}`, `K{VAR.}`, `K{VAR-}` in the template string.
 /// - If the variable `VAR` has a non-empty value, it replaces the placeholder with `value + suffix`.
 /// - If the variable `VAR` has an empty value (`""`), it replaces the entire placeholder (including the suffix) with an empty string.
 pub fn substitute<T>(template: T, variables: &HashMap<String, String>) -> Result<String, Error>
@@ -53,8 +53,8 @@ where
 
     validate_vars(variables)?;
 
-    // Regular expression to match placeholders like ${VAR}, ${VAR.}, ${VAR-}
-    let re = Regex::new(r"\$\{([A-Za-z_][A-Za-z0-9_]*)([\.\-][^}]*)?\}").unwrap();
+    // Regular expression to match placeholders like K{VAR}, K{VAR.}, K{VAR-}
+    let re = Regex::new(r"K\{([A-Za-z_][A-Za-z0-9_]*)([\.\-][^}]*)?\}").unwrap();
 
     output = re
         .replace_all(&output, |caps: &regex::Captures| {
@@ -82,7 +82,7 @@ pub fn is_templated<S>(input: S) -> bool
 where
     S: AsRef<str>,
 {
-    let re = Regex::new(r"\$\{([A-Za-z_][A-Za-z0-9_]*)([\.\-][^}]*)?\}").unwrap();
+    let re = Regex::new(r"K\{([A-Za-z_][A-Za-z0-9_]*)([\.\-][^}]*)?\}").unwrap();
     re.is_match(input.as_ref())
 }
 
@@ -90,7 +90,7 @@ where
 ///
 /// This check whether substitution variables are valid. In order to make
 /// substitution deterministic, the following characters are not allowed
-/// within variables names nor values: `$`, `{`, `}`.
+/// within variables names nor values: `K`, `{`, `}`.
 pub fn validate_vars(variables: &HashMap<String, String>) -> Result<(), Error> {
     for (k, v) in variables {
         validate(k, "key")?;
@@ -104,7 +104,7 @@ fn validate<S>(value: S, kind: &str) -> Result<(), Error>
 where
     S: AsRef<str>,
 {
-    let forbidden = &["$", "{", "}"];
+    let forbidden = &["K{", "{", "}"];
     for c in forbidden {
         if value.as_ref().contains(c) {
             let err_msg = format!(
@@ -126,7 +126,7 @@ mod tests {
 
     #[test]
     fn basic_subst() {
-        let template = "foo ${VAR} bar";
+        let template = "foo K{VAR} bar";
         let mut env = HashMap::new();
         env.insert("VAR".to_string(), "var".to_string());
 
@@ -140,10 +140,13 @@ mod tests {
         let plain = "foo";
         assert!(!is_templated(plain));
 
-        let template = "foo ${VAR} bar";
+        let template = "foo K{VAR} bar";
         assert!(is_templated(template));
 
-        let starting = "foo${";
+        let template = "foo k{VAR} bar";
+        assert!(!is_templated(template));
+
+        let starting = "fooK{";
         assert!(!is_templated(starting));
 
         let ending = "foo}";
@@ -152,7 +155,7 @@ mod tests {
 
     #[test]
     fn basic_empty_vars() {
-        let template = "foo ${VAR} bar";
+        let template = "foo K{VAR} bar";
         let env = HashMap::new();
 
         let out = substitute(template, &env).unwrap();
@@ -161,7 +164,7 @@ mod tests {
 
     #[test]
     fn dollar_bracket() {
-        let template = "foo ${ bar";
+        let template = "foo K{ bar";
         let mut env = HashMap::new();
         env.insert("VAR".to_string(), "var".to_string());
 
@@ -171,21 +174,26 @@ mod tests {
 
     #[test]
     fn invalid_vars() {
-        let template = "foo ${VAR} bar";
+        let template = "foo K{VAR} bar";
         let mut env = HashMap::new();
-        env.insert("${VAR}".to_string(), "var".to_string());
+        env.insert("K{VAR}".to_string(), "var".to_string());
 
         substitute(template, &env).unwrap_err();
 
         let mut env = HashMap::new();
-        env.insert("VAR".to_string(), "${VAR}".to_string());
+        env.insert("VAR".to_string(), "K{".to_string());
 
         substitute(template, &env).unwrap_err();
+
+        let mut env = HashMap::new();
+        env.insert("VAR".to_string(), "K".to_string());
+
+        substitute(template, &env).unwrap();
     }
 
     #[test]
     fn test_substitute_with_suffix_non_empty_var() {
-        let template = "${VAR} ${VAR.} ${VAR-}";
+        let template = "K{VAR} K{VAR.} K{VAR-}";
         let mut variables = HashMap::new();
         variables.insert("VAR".to_string(), "hoge".to_string());
 
@@ -195,7 +203,7 @@ mod tests {
 
     #[test]
     fn test_substitute_with_suffix_empty_var() {
-        let template = "${VAR} ${VAR.} ${VAR-}";
+        let template = "K{VAR} K{VAR.} K{VAR-}";
         let mut variables = HashMap::new();
         variables.insert("VAR".to_string(), "".to_string());
 
@@ -205,16 +213,16 @@ mod tests {
 
     #[test]
     fn test_substitute_with_missing_var() {
-        let template = "${VAR} ${VAR.} ${VAR-}";
+        let template = "K{VAR} K{VAR.} K{VAR-}";
         let variables = HashMap::new();
 
         let result = substitute(template, &variables).unwrap();
-        assert_eq!(result, "${VAR} ${VAR.} ${VAR-}");
+        assert_eq!(result, "K{VAR} K{VAR.} K{VAR-}");
     }
 
     #[test]
     fn test_substitute_with_complex_suffix() {
-        let template = "${VAR.suffix} ${VAR-extra}";
+        let template = "K{VAR.suffix} K{VAR-extra}";
         let mut variables = HashMap::new();
         variables.insert("VAR".to_string(), "value".to_string());
 
